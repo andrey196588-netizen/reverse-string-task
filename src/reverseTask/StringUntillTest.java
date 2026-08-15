9befb40bdd9b2d50256d3015213360a868cd1757
andrey196588-netizen <andrey196588@gmail.com>
2026-08-15 17:26:40 +0300

    Update StringUntillTest.java

--- src/reverseTask/StringUntillTest.java
+++ src/reverseTask/StringUntillTest.java
@@ -6,14 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringUntillTest {
 
     @Test
-    void testReverseLetters() {
+    void shouldReverseStandardString() {
         assertEquals("t@eb eht av$J!123", StringUntill.reverseLetters("J@va the be$t!123"));
+    }
+    @Test
+    void returnsEmptyForEmptyInput() {
         assertEquals("", StringUntill.reverseLetters(""));
+    }
+    @Test
+    void shouldHandleSingleLetter() {
         assertEquals("a", StringUntill.reverseLetters("a"));
+    }
+
+    @Test
+    void keepsNonLettersInPlace() {
         assertEquals("123 @#!", StringUntill.reverseLetters("123 @#!"));
-        assertEquals("dcba", StringUntill.reverseLetters("abcd"));
         assertEquals("!c@b#a$", StringUntill.reverseLetters("!a@b#c$"));
+    }
+    @Test
+    void reversesOnlyLetters() {
+        assertEquals("dcba", StringUntill.reverseLetters("abcd"));
+    }
+    @Test
+    void shouldPreserveCaseWhenReversing() {
         assertEquals("A@b", StringUntill.reverseLetters("b@A"));
+    }
+    @Test
+    void shouldReturnNullForNullInput() {
         assertNull(StringUntill.reverseLetters(null));
     }
 }
